ddee86a3c6203a83bce6cc4cd8e561b081a44ac1
Ometere <ookuoluse@yahoo.com>
2023-11-16 12:59:19 +0100

    contact page

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PageIntro from '../../../components/PageIntro'
+
+export default function ContactPage() {
+  return (
+    <PageIntro eyebrow="Contact" title="Let's work together">
+        <p>We can not wait to hear from you</p>
+    </PageIntro>
+  )
+}
