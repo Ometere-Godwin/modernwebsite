eb40e92a80f635f0d0d976fdfd34b41fe363004d
Ometere <ookuoluse@yahoo.com>
2023-11-16 12:52:29 +0100

    blog post

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageIntro from '../../../components/PageIntro'
+
+export default function BlogPage() {
+  return (
+    <PageIntro eyebrow="Blog" title="The latest articles and news">
+        <p>
+            Stay up-to-date with the latest industry news as our marketing team finds new ways to re-purpose old CSS tricks articles
+        </p>
+    </PageIntro>
+  )
+}
