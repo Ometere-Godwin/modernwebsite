3114632e292f5f1d4a931f04334bf7ab32457c72
Ometere <ookuoluse@yahoo.com>
2023-11-16 12:41:51 +0100

    process page

---
+++ src/app/process/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageIntro from '../../../components/PageIntro'
+
+export default function ProcessPage() {
+  return (
+    <PageIntro eyebrow="Our process" title="Our we work">
+      <p>
+        We believe in efficiency and maximizing our resources to provide the bestvalue to our
+        clients. The primary way to do that is by re-using the same five projects we have 
+        been developing for the past decade.
+      </p>
+    </PageIntro>
+  )
+}
