d1dc1284cdae8311c299e1d460267fb500e26627
Ometere <ookuoluse@yahoo.com>
2023-11-16 12:00:54 +0100

    page-not-found

---
+++ src/app/not-found.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Container from '../../components/Container'
+import FadeIn from '../../components/FadeIn'
+import Link from 'next/link'
+
+export default function NotFound() {
+  return (
+    <Container>
+        <FadeIn className= "flex items-center justify-center flex-col">
+            <p className='font-display text-4xl font-semibold text-neutral-950
+            sm:'>
+                404
+            </p>
+            <h1 className='mt-2 font-display text-2xl font-semibold text-neutral-950'>
+                Page not found
+            </h1>
+            <p className='mt-2 text-sm text-neutral-600'>
+                Sorry, we could not find the page you are looking for.
+            </p>
+            <Link 
+            href={"/"}
+            className='mt-4 text-sm font-semibold text-neutral-950 transition
+            hover:text-neutral-700 border-2 border-neutral-300 px-1 rounded'> Go to home
+            </Link>
+        </FadeIn>
+    </Container>
+  )
+}
