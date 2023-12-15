8de58eee5b31637ee51cbdde5e8e0d1b31b49d60
Ometere <ookuoluse@yahoo.com>
2023-12-15 05:56:52 +0100

    contact details

--- components/ContactDetails.jsx
+++ components/ContactDetails.jsx
@@ -8,42 +8,43 @@ import SocialMedia from './SocialMedia'
 export default function ContactDetails() {
   return (
     <FadeIn>
-        <h2 className='font-display text-base font-semibold text-neutral-950'>
-            Our Offices
+      <h2 className="font-display text-base font-semibold text-neutral-950">
+        Our offices
+      </h2>
+      <p className="mt-6 text-base text-neutral-600">
+        Prefer doing things in person? We don’t but we have to list our
+        addresses here for legal reasons.
+      </p>
+      <Offices className="mt-10 grid grid-cols-1 gap-8 sm:grid-cols-2" />
+      <Border className="mt-16 pt-16">
+        <h2 className="font-display text-base font-semibold text-neutral-950">
+          Email us
         </h2>
-
-        <p className='mt-6 text-neutral-600 text-justify'>
-            Prefer doing things in person? We don't but we have to list
-            out our addresses here for legal reasons.
-        </p>
-        <Offices className = "mt-10 grid grid-cols-1 gap-8 sm:grid-cols-2"/>
-        <Border className="mt-16 pt-16">
-            <h2 className='font-display text-base font-semibold text-neutral-950'>
-                Email us
-            </h2>
-            <dl className='mt-6 grid grid-cols-1 gap-8 text-sm sm:grid-cols-2'>
-                {[
-                    ["Careers", "ookuoluse@yahoo.com"],
-                    ["press", "ookuoluse@gmail.com"],
-                ].map(([label, email]) => (
-                    <div key={email}>
-                        <dt className='font-semibold text-neutral-950'>{label}</dt>
-                        <dd>
-                            <Link href={`mailto:${email}`}
-                            className='text-neutral-950 hover:text-neutral-950'>
-                                {email}
-                            </Link>
-                        </dd>
-                    </div>
-                ))}
-            </dl>
-        </Border>
-        <Border className="mt-16 pt-16">
-            <h2 className='font-display text-base font-semibold text-neutral-950'>
-                Follow Us
-            </h2>
-            <SocialMedia className="mt-6"/>
-        </Border>
+        <dl className="mt-6 grid grid-cols-1 gap-8 text-sm sm:grid-cols-2">
+          {[
+            ["Careers", "reactjsbd.com"],
+            ["Press", "noorjsdivs@gmail.com"],
+          ].map(([label, email]) => (
+            <div key={email}>
+              <dt className="font-semibold text-neutral-950">{label}</dt>
+              <dd>
+                <Link
+                  href={`mailto:${email}`}
+                  className="text-neutral-600 hover:text-neutral-950"
+                >
+                  {email}
+                </Link>
+              </dd>
+            </div>
+          ))}
+        </dl>
+      </Border>
+      <Border className="mt-16 pt-16">
+        <h2 className="font-display text-base font-semibold text-neutral-950">
+          Follow us
+        </h2>
+        <SocialMedia className="mt-6" />
+      </Border>
     </FadeIn>
   )
 }
