d52ee4f5e8cb2c26369215470df7b27236f734fe
Ometere <ookuoluse@yahoo.com>
2023-12-15 06:08:52 +0100

    Contact

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -7,45 +7,41 @@ import Button from "./Button";
 export default function () {
   return (
     <FadeIn>
-      <form>
-        <h2 className="font-display text-base font-semibold text-neutral-950">
-          Work Inquiries
-        </h2>
-        <div className="isolate mt-6 -space-y-px rounded-2xl bg-white/50">
-          <TextInput label="Name" name="name" autoComplete="none" />
-          <TextInput
-            label="Email"
-            type="email"
-            name="email"
-            autoComplete="email"
-          />
-
-          <TextInput
-            label="Company"
-            name="company"
-            autoComplete="organization"
-          />
-        </div>
-
+    <form>
+      <h2 className="font-display text-base font-semibold text-neutral-950">
+        Work inquiries
+      </h2>
+      <div className="isolate mt-6 -space-y-px rounded-2xl bg-white/50">
+        <TextInput label="Name" name="name" autoComplete="name" />
+        <TextInput
+          label="Email"
+          type="email"
+          name="email"
+          autoComplete="email"
+        />
+        <TextInput
+          label="Company"
+          name="company"
+          autoComplete="organization"
+        />
         <TextInput label="Phone" type="tel" name="phone" autoComplete="tel" />
-
         <TextInput label="Message" name="message" />
-
-        <div className="border border-neutral-300 px-6 first:rounded-t-2xl last:rounded-b-2xl">
-        <fieldset>
-          <legend className="text-base/6 text-neutral-500">Budget</legend>
-        </fieldset>
-        <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 gap-8 mb-6">
-              <RadioInput label="$25K – $50K" name="budget" value="25" />
-              <RadioInput label="$50K – $100K" name="budget" value="50" />
-              <RadioInput label="$100K – $150K" name="budget" value="100" />
-              <RadioInput label="More than $150K" name="budget" value="150" />
-            </div>
+        <div className="border border-neutral-300 px-6 py-8 first:rounded-t-2xl last:rounded-b-2xl">
+          <fieldset>
+            <legend className="text-base/6 text-neutral-500">Budget</legend>
+          </fieldset>
+          <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 gap-8">
+            <RadioInput label="$25K – $50K" name="budget" value="25" />
+            <RadioInput label="$50K – $100K" name="budget" value="50" />
+            <RadioInput label="$100K – $150K" name="budget" value="100" />
+            <RadioInput label="More than $150K" name="budget" value="150" />
+          </div>
         </div>
-        <Button type = "submit" className= "mt-10">
-          Let's work together
-        </Button>
-      </form>
-    </FadeIn>
+      </div>
+      <Button type="submit" className="mt-10">
+        Let’s work together
+      </Button>
+    </form>
+  </FadeIn>
   );
 }
